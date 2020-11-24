1113ab6a7db5523600d2d75f291d0510c6cf7480
danybb2020 <daniel.balderas@gmail.com>
2020-11-24 13:24:19 -0800

    Initial deploy

---
+++ client/build/precache-manifest.5c4f905ec9c394a77e87919175f77eef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b37c4d37d2d5088caa52",
+    "url": "/static/css/main.c5e3aa90.chunk.css"
+  },
+  {
+    "revision": "b37c4d37d2d5088caa52",
+    "url": "/static/js/main.0a6871b4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f14e103a85ded4a2db8",
+    "url": "/static/js/2.9f08725b.chunk.js"
+  },
+  {
+    "revision": "766f6a07b47a85c3b4f5f33fbf4cd151",
+    "url": "/index.html"
+  }
+];
